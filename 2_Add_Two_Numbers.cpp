7bd05e46371720845b159a5841747f5487ca276e
AaryaKale0607 <kaleaarya704@gmai.com>
2026-03-26 14:21:17 +0530

    Add the two numbers and return the sum as a linked list.

---
+++ 2_Add_Two_Numbers.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+         ListNode* dummy = new ListNode(0); // dummy node
+        ListNode* current = dummy;
+        
+        int carry = 0;
+        
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int sum = carry;
+            
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+        }
+        
+        return dummy->next;
+    }
+};
