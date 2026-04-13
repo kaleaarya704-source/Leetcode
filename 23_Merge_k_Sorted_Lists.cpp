eff2968f6d9796471dd4a2a6c131d215c960d331
AaryaKale0607 <kaleaarya704@gmail.com>
2026-04-13 07:51:20 +0530

    Merge k number of sorted linked lists and return one linked list

---
+++ 23_Merge_k_Sorted_Lists.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <queue>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+using namespace std;
+
+class Solution {
+public:
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val; // min-heap
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+
+        // Push initial nodes
+        for (auto node : lists) {
+            if (node) pq.push(node);
+        }
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (!pq.empty()) {
+            ListNode* smallest = pq.top();
+            pq.pop();
+
+            tail->next = smallest;
+            tail = tail->next;
+
+            if (smallest->next) {
+                pq.push(smallest->next);
+            }
+        }
+
+        return dummy->next;
+    }
+};
